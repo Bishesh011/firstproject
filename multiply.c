53e601cf37c7842579ddbcde544a1264c93d472d
Bises Chhetri <biseschhetri17@gmail.com>
2025-09-18 17:57:05 +0545

    all

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int a,b,c,product;
+    printf("Enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    product=a*b*c;
+    printf("The product is %d\n",product);
+    return 0;
+}
